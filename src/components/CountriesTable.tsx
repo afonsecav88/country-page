eee32980a5e58368b8fbd2d5eefdcdb2c64d7200
Adrian Fonseca Vega <afonsecav88@gmail.com>
2025-01-29 00:22:21 -0300

    refactor: improve layout and responsiveness of CountriesTable component; add no data message

--- src/components/CountriesTable.tsx
+++ src/components/CountriesTable.tsx
@@ -1,39 +1,48 @@
 import { CountriesInfo } from '../interfaces/CountriesInfo.interface';
 
 type CountriesTableProps = { paginatedCountries: CountriesInfo[] };
-
 export const CountriesTable = ({ paginatedCountries }: CountriesTableProps) => {
   return (
-    <table className="table-fixed h-[41rem] w-full lg:w-[55vw]">
-      <thead>
-        <tr className="text-xs text-[#6C727F] font-bold">
-          <td className="min-w-20   pb-5">Flag</td>
-          <td className="w-40 min-w-48 min-h-12 pb-5">Name</td>
-          <td className="min-w-20 pb-5">Population</td>
-          <td className="min-w-20 pb-5">Area(km²)</td>
-          <td className="min-w-32 pb-5">Region</td>
-        </tr>
-        <tr className="border-[#282B30] border-2 lg:flex-row" />
-      </thead>
-      <tbody>
-        {paginatedCountries.map(({ flags, name, population, area, region }) => (
-          <tr
-            key={name.common}
-            className="table-row text-base  min-w-full text-[#D2D5DA]">
-            <td className=" min-w-20 pt-4 ">
-              <img
-                src={flags.png}
-                alt={name.common}
-                className="w-12 h-8 rounded-sm"
-              />
-            </td>
-            <td className="w-48 min-w-48 min-h-12  pt-4">{name.common}</td>
-            <td className="min-w-20 pt-4">{population}</td>
-            <td className="min-w-20 pt-4">{area}</td>
-            <td className="min-w-32 pt-4">{region}</td>
+    <div className="w-full lg:w-[55vw] overflow-x-auto">
+      <table className="table-fixed w-full">
+        <thead>
+          <tr className="text-xs text-[#6C727F] font-bold">
+            <td className="w-20 pb-5">Flag</td>
+            <td className="w-56 pb-5">Name</td>
+            <td className="min-w-20 pb-5">Population</td>
+            <td className="min-w-20 pb-5">Area(km²)</td>
+            <td className="min-w-32 pb-5">Region</td>
           </tr>
-        ))}
-      </tbody>
-    </table>
+          <tr className="border-[#282B30] border-2" />
+        </thead>
+        <tbody className="min-h-[200px] h-auto align-top">
+          {paginatedCountries.length > 0 ? (
+            paginatedCountries.map(
+              ({ flags, name, population, area, region }) => (
+                <tr key={name.common} className="text-base text-[#D2D5DA]">
+                  <td className="min-w-20 h-16 pt-4">
+                    <img
+                      src={flags.png}
+                      alt={name.common}
+                      className="w-12 h-8 rounded-sm"
+                    />
+                  </td>
+                  <td className="pt-4 h-16">{name.common}</td>
+                  <td className="pt-4 h-16">{population}</td>
+                  <td className="pt-4 h-16">{area}</td>
+                  <td className="pt-4 h-16">{region}</td>
+                </tr>
+              )
+            )
+          ) : (
+            <tr>
+              <td colSpan={5} className="text-center py-4 text-gray-400">
+                No data available
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
