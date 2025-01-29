a16d750881bcf6e827b40a5a04f30261dd5d837f
Adrian Fonseca Vega <afonsecav88@gmail.com>
2025-01-29 13:59:30 -0300

    refactor: adjust ContainerCard layout for improved responsiveness and spacing

--- src/components/ContainerCard.tsx
+++ src/components/ContainerCard.tsx
@@ -10,9 +10,9 @@ import { RegionTags } from './RegionTags';
 export const ContainerCard: FC = () => {
   return (
     <section
-      className="grid grid-flow-col grid-rows-[20vw_minmax(20rem,auto)] md:grid-rows-[4vw_minmax(auto,auto)] gap-5 p-2 sm:pt-6 bg-[#1C1D1F] text-[#D2D5DA]  w-[94vw] md:w-[96vw]  lg:w-[93vw] xl:w-[83vw] absolute top-48 rounded-lg
+      className="grid grid-flow-col grid-rows-[20vw_minmax(20rem,auto)] md:grid-rows-[4vw_minmax(auto,auto)] gap-5 p-2 sm:pt-6 bg-[#1C1D1F] text-[#D2D5DA]  w-[94vw] md:w-[96vw]  lg:w-[93vw] xl:w-[88vw] absolute top-48 rounded-lg
      border-[#282B30] border pb-8">
-      <article className="inline-grid grid-flow-row grid-cols-1 md:grid-flow-col justify-between sm:px-8 pt-2 gap-4">
+      <article className="inline-grid grid-flow-row grid-cols-1 md:grid-flow-col justify-between sm:px-10 pt-2 gap-4">
         <CountriesCount />
         <SearchByFilter />
       </article>
