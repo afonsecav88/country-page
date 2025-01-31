a7f89e4b74e6e24924394c6fac8f1bc8c9d9106b
Adrian Fonseca Vega <afonsecav88@gmail.com>
2025-01-31 14:29:47 -0300

    refactor: enhance layout and styling in CountryDetails component; improve responsiveness and readability of population and area displays

--- src/components/CountryDetails.tsx
+++ src/components/CountryDetails.tsx
@@ -29,15 +29,15 @@ export const CountryDetails = ({ country }: CountryDetailsProps) => {
 
   return (
     <section
-      className="grid grid-flow-col grid-rows-[20vw_minmax(20rem,auto)] md:grid-rows-[4vw_minmax(auto,auto)] gap-5  sm:pt-6 bg-[#1C1D1F] text-[#D2D5DA]  w-full md:w-[70vw]  lg:w-[56.5vw]  h-[59rem] absolute top-52 rounded-lg
+      className="grid grid-flow-col grid-rows-[20vw_minmax(20rem,auto)] md:grid-rows-[4vw_minmax(auto,auto)] gap-5  sm:pt-6 bg-[#1C1D1F] text-[#D2D5DA] w-full md:w-[70vw] lg-w-[56.5vw] max-w-[45rem] h-auto absolute top-52 rounded-lg
   border-[#282B30] border pb-8">
-      <div className="flex flex-col">
+      <div className="flex flex-col  flex-wrap">
         <div className="flex justify-center pb-16 relative -top-16">
           {' '}
           <img
             src={flags.svg}
             alt={name.common}
-            className="w-64 h-44 rounded-2xl"
+            className="min-w-64 h-44 rounded-2xl"
           />
         </div>
       </div>
@@ -46,53 +46,58 @@ export const CountryDetails = ({ country }: CountryDetailsProps) => {
           <p className="text-3xl font-bold">{name.common}</p>
           <p className="text-base font-semibold">{name.official}</p>
         </div>
-        <div className="flex gap-4 justify-center pb-6">
-          <span className="inline-flex bg-[#282B30] gap-3 p-4 rounded-md w-fit items-center">
+        <div className="flex gap-10 flex-col items-center sm:flex-row justify-center pb-6">
+          <span className="inline-flex bg-[#282B30] gap-3 p-3 rounded-md w-fit items-center">
             <p className="text-sm font-semibold">Population</p>
             <div className="border-[#1E1F23] min-h-8 border"></div>
             <p className="text-md font-semibold">
               {population.toLocaleString()}
             </p>
           </span>
-          <span>Area(km²)</span>
-          <span>{area.toLocaleString()}</span>
+          <span className="inline-flex bg-[#282B30] gap-3 p-3 rounded-md w-fit items-center">
+            <p className="text-sm font-semibold">Area(km²)</p>
+            <div className="border-[#1E1F23] min-h-8 border"></div>
+            <p className="text-md font-semibold">{area.toLocaleString()}</p>
+          </span>
         </div>
         <hr className="border-[#23262b] border w-full" />
-        <div className="inline-flex justify-between px-6 py-6 items-center">
-          <p>Capital</p>
-          <p>{capital}</p>
+        <div className="inline-flex   justify-around sm:justify-between px-6 py-6 items-center">
+          <p className="text-sm font-semibold">Capital</p>
+          <p className="text-sm font-semibold">{capital}</p>
         </div>
         <hr className="border-[#23262b] border w-full" />
-        <div className="inline-flex justify-between px-6 py-6 items-center">
-          <p>Subregion</p>
-          <p>{subregion}</p>
+        <div className="inline-flex  justify-around sm:justify-between px-6 py-6 items-center">
+          <p className="text-sm font-semibold">Subregion</p>
+          <p className="text-sm font-semibold">{subregion}</p>
         </div>
         <hr className="border-[#23262b] border w-full" />
-        <div className="inline-flex justify-between px-6 py-6 items-center">
-          <p>Language</p>
+        <div className="inline-flex  justify-around sm:justify-between px-6 py-6 items-center">
+          <p className="text-sm font-semibold">Language</p>
           {Object.values(languages).map((language) => (
-            <p key={language}>{language}</p>
+            <p key={language} className="text-sm font-semibold">
+              {language}
+            </p>
           ))}
         </div>
         <hr className="border-[#23262b] border w-full" />
-        <div className="inline-flex justify-between px-6 py-6 items-center">
-          <p>Currencies</p>
+        <div className="inline-flex  justify-around sm:justify-between px-6 py-6 items-center">
+          <p className="text-sm font-semibold">Currencies</p>
           {Object.values(currencies).map((c) => (
             <p key={c.name}>{c.name}</p>
           ))}
         </div>
         <hr className="border-[#23262b] border w-full" />
-        <div className="inline-flex justify-between px-6 py-6 items-center">
-          <p>Continents</p>
+        <div className="inline-flex  justify-around sm:justify-between px-6 py-6 items-center">
+          <p className="text-sm font-semibold">Continents</p>
           {continents.map((continent) => (
             <p key={continent}>{continent}</p>
           ))}
         </div>
         <hr className="border-[#23262b] border w-full" />
-        <div className="inline-flex justify-between px-6 py-6 items-center">
-          <p>Neighboring Countries</p>
+        <div className="inline-flex  justify-around sm:justify-between px-6 py-6 items-center">
+          <p className="text-sm font-semibold">Neighboring Countries</p>
         </div>
-        <div className="flex px-6 py-6 justify-between gap-4 flex-wrap">
+        <div className="flex px-6 py-6 gap-4 flex-wrap">
           {neightbouringCountries.map((neighbouring) => (
             <div
               key={neighbouring?.name.common}
@@ -102,7 +107,9 @@ export const CountryDetails = ({ country }: CountryDetailsProps) => {
                 alt={neighbouring?.name.common}
                 className="w-20 max-h-12 rounded-sm"
               />
-              <p> {neighbouring?.name.common}</p>
+              <p className="text-xs font-semibold pt-2">
+                {neighbouring?.name.common}
+              </p>
             </div>
           ))}
         </div>
