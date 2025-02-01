2097eadbd741ffa92e97fd2d02b1a9e602e859b2
Adrian Fonseca Vega <afonsecav88@gmail.com>
2025-02-01 00:14:14 -0300

    refactor: add loading skeleton to CountryDetails component for improved user experience during data fetching

--- src/components/CountryDetails.tsx
+++ src/components/CountryDetails.tsx
@@ -3,6 +3,7 @@ import { useGetNeightbouringCountries } from '../hooks/useGetNeightbouringCountr
 import { CountriesInfo } from '../interfaces/CountriesInfo.interface';
 import { useNavigate } from 'react-router-dom';
 import { useFormattedCountryName } from '../hooks/useFormattedName';
+import { LoadingCountryDetail } from './CountryItem';
 
 type CountryDetailsProps = {
   country: CountriesInfo;
@@ -33,9 +34,10 @@ export const CountryDetails = ({ country }: CountryDetailsProps) => {
 
   return (
     <section
-      className="grid grid-flow-col grid-rows-[20vw_minmax(20rem,auto)] md:grid-rows-[4vw_minmax(auto,auto)] gap-5  sm:pt-6 bg-[#1C1D1F] text-[#D2D5DA] w-full md:w-[70vw] lg-w-[56.5vw] max-w-[45rem] h-auto absolute top-52 rounded-lg
+      className="grid grid-flow-col grid-rows-[20vw_minmax(20rem,auto)] md:grid-rows-[4vw_minmax(auto,auto)] gap-5  sm:pt-6 bg-[#1C1D1F] text-[#D2D5DA] w-full md:w-[70vw] lg-w-[56.5vw] max-w-[45rem] h-auto min-h-[90vh] absolute top-52 rounded-lg
   border-[#282B30] border pb-8">
-      <div className="flex flex-col  flex-wrap">
+      {country && <LoadingCountryDetail />}
+      {/* <div className="flex flex-col  flex-wrap">
         <div className="flex justify-center pb-2 sm:pb-10 relative -top-16">
           {' '}
           <img
@@ -128,7 +130,7 @@ export const CountryDetails = ({ country }: CountryDetailsProps) => {
             </button>
           ))}
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
--- src/components/CountryItem.tsx
+++ src/components/CountryItem.tsx
@@ -1,3 +1,48 @@
-export const CountryItem = () => {
-  return <div>CountryItem</div>;
+import Skeleton from 'react-loading-skeleton';
+
+export const LoadingCountryDetail = () => {
+  return (
+    <div className="flex justify-center w-[55vw]">
+      <div className="flex flex-col justify-center items-center sm:pb-10 relative top-80">
+        <Skeleton
+          height={150}
+          width={100}
+          count={1}
+          className="skeleton min-w-64 h-44 rounded-2xl"
+        />
+        <div className="flex pt-8">
+          <Skeleton height={80} width={150} count={1} className="skeleton" />
+        </div>
+        <div className="inline-flex gap-28 mb-14 pt-8">
+          <Skeleton height={40} width={200} count={1} className="skeleton " />
+          <Skeleton height={40} width={200} count={1} className="skeleton" />
+        </div>
+        <div className="inline-flex w-[55vw]  justify-start pl-10 gap-40 sm:justify-between sm:px-6 py-6 items-center">
+          <Skeleton height={20} width={100} count={1} className="skeleton" />
+          <Skeleton height={20} width={100} count={1} className="skeleton" />
+        </div>
+        <div className="inline-flex w-[55vw]  justify-start pl-10 gap-40 sm:justify-between sm:px-6 py-6 items-center">
+          <Skeleton height={20} width={100} count={1} className="skeleton" />
+          <Skeleton height={20} width={100} count={1} className="skeleton" />
+        </div>
+        <div className="inline-flex w-[55vw]  justify-start pl-10 gap-40 sm:justify-between sm:px-6 py-6 items-center">
+          <Skeleton height={20} width={100} count={1} className="skeleton" />
+          <Skeleton height={20} width={100} count={1} className="skeleton" />
+        </div>
+        <div className="inline-flex w-[55vw] justify-start pl-10 gap-40 sm:justify-between sm:px-6 py-6 items-center">
+          <Skeleton height={20} width={100} count={1} className="skeleton" />
+          <Skeleton height={20} width={100} count={1} className="skeleton" />
+        </div>
+        <div className="inline-flex w-[55vw]  justify-start pl-10 gap-40 sm:justify-between sm:px-6 py-6 items-center">
+          <Skeleton height={20} width={100} count={1} className="skeleton" />
+          <Skeleton height={20} width={100} count={1} className="skeleton" />
+        </div>
+        <div className="flex justify-start gap-6">
+          <Skeleton height={80} width={150} count={1} className="skeleton" />
+          <Skeleton height={80} width={150} count={1} className="skeleton" />
+          <Skeleton height={80} width={150} count={1} className="skeleton" />
+        </div>
+      </div>
+    </div>
+  );
 };
