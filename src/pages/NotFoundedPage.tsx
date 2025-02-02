c85c778fa5ddc1478cf01b21a04f6ea37c4a8f41
Adrian Fonseca Vega <afonsecav88@gmail.com>
2025-02-02 00:47:45 -0300

    refactor: update NotFoundedPage layout and navigation for improved user experience

--- src/pages/NotFoundedPage.tsx
+++ src/pages/NotFoundedPage.tsx
@@ -1,17 +1,20 @@
 import { FC } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const NotFoundedPage: FC = () => {
-  const navigate = useNavigate();
   return (
-    <div className="text-[#D2D5DA]">
-      <div className="inline-flex gap-4 items-center p-5">
-        <p>Not FoundedPage</p>
-        <button
-          onClick={() => navigate('/')}
-          className="bg-blue-500 p-2 rounded-md">
-          ⬅ Go Home
-        </button>
+    <div className="flex text-[#D2D5DA] pt-12">
+      <div className="flex flex-col items-center text-center align-middle top-20 ">
+        <p className="text-blue-500 text-7xl u">Página no Encontrada</p>
+        <p className="text-blue-500 text-9xl text-center font-extrabold">404</p>
+        <p className="text-amber-50 text-xl font-bold  bg-slate-500 rounded-full py-2 w-2/3">
+          Lo sentimos, la página no fue encontrada !
+        </p>
+        <Link
+          to="/"
+          className="flex justify-center text-slate-600 txt-3xl w-fit mt-4 bg-cyan-400 py-2 px-6 font-bold rounded-md hover:bg-cyan-600 hover:text-amber-100 transition duration-200 ease-linear">
+          Regresar
+        </Link>
       </div>
     </div>
   );
